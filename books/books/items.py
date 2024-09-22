208101a16045859fdbe38830c79b5860e33dad4f
António Lopes <47214141+trailtony@users.noreply.github.com>
2024-09-22 17:58:30 +0100

    Update items.py

--- books/books/items.py
+++ books/books/items.py
@@ -7,6 +7,6 @@ import scrapy
 
 
 class BooksItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
